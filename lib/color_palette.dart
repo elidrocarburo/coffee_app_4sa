c9b393a7cd09174a0256eadd90afd87c4ec738d4
Adriana Cervera Rivadeneyra <princeritsu2209@outlook.com>
2025-04-19 10:44:02 -0600

    added coffee list

--- lib/color_palette.dart
+++ lib/color_palette.dart
@@ -3,4 +3,6 @@ import 'package:flutter/material.dart';
 class ColorPalette {
   final Color scaffoldBg = Color(0xFF0D0F14);
   final Color searchBarFill = Color(0xFF141921);
+  final Color coffeeSelected = Color(0xFFD17741);
+  final Color coffeeUnselected = Color(0xFF525559);
 }
--- lib/dashboard_page.dart
+++ lib/dashboard_page.dart
@@ -1,5 +1,6 @@
 import 'package:coffee_app_4sa/color_palette.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:iconify_flutter/iconify_flutter.dart';
 import 'package:iconify_flutter/icons/heroicons_solid.dart';
@@ -12,6 +13,17 @@ class DashboardPage extends StatefulWidget {
 }
 
 class _DashboardPageState extends State<DashboardPage> {
+
+  final List<String> coffeeTypes = [
+    'Cappuccino',
+    'Espresso',
+    'Latte',
+    'Flat White'
+  ];
+
+  String selectedItem = 'Cappuccino';
+  int counter = -1;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -67,11 +79,81 @@ class _DashboardPageState extends State<DashboardPage> {
               )),
             ),
             SizedBox(height: 20.0),
-            SearchBar()
-
+            SearchBar(),
+            SizedBox(height: 20.0),
+            Padding(padding: EdgeInsets.only(left: 15.0, right: 15.0, top: 15.0),
+            child: ShaderMask(
+              shaderCallback: (Rect bounds) {
+                return LinearGradient(
+                  begin: Alignment(0.7, -1.0),
+                  end: Alignment(1.0, -1.0),
+                  colors: <Color>[Colors.black, Colors.transparent]
+                ).createShader(bounds);
+              },
+              blendMode: BlendMode.dstATop,
+              child: Container(
+                color: Color(0xFF0D0F14),
+                width: MediaQuery.of(context).size.width - 20.0,
+                height: 40.0,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    ...coffeeTypes.map((e) {
+                      counter++;
+                      if(counter<=3) return _buildTypes(e,counter);
+                      else {
+                        counter = 0;
+                        return _buildTypes(e, counter);
+                      }
+                    }).toList()
+                  ],
+                ),
+              ),
+              ),
+            )
           ],
         ),
       )
+    );
+  }
+
+  Widget _buildTypes(coffee, counter) {
+    return Padding(
+      padding: counter !=0 
+      ? EdgeInsets.only(left: 25.0)
+      : EdgeInsets.only(left: 7.0),
+      child: Container(
+        height: 50.0,
+        color: Color(0xFF0D0F14),
+        child: Column(
+          children: [
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  selectedItem = coffee;
+                });
+              },
+              child: Text(coffee,
+              style: GoogleFonts.sourceSansPro(
+                fontWeight: FontWeight.bold,
+                color: coffee == selectedItem ?
+                ColorPalette().coffeeSelected : ColorPalette().coffeeUnselected,
+                fontSize: 20.0
+              ),
+              ),
+            ),
+            SizedBox(height: 4.0),
+            Container(
+              height: 8.0,
+              width: 8.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(4.0),
+                color: coffee == selectedItem ? ColorPalette().coffeeSelected : Colors.transparent
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
