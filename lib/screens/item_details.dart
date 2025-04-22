c19c21a5f1e5a8d18314e3739e3c2cf8d50c5b83
Adriana Cervera Rivadeneyra <princeritsu2209@outlook.com>
2025-04-22 17:46:11 -0600

    finish the description box of item_details.dart

--- lib/screens/item_details.dart
+++ lib/screens/item_details.dart
@@ -1,9 +1,15 @@
+import 'package:coffee_app_4sa/color_palette.dart';
 import 'package:coffee_app_4sa/models/coffee_item.dart';
+import 'package:colorful_iconify_flutter/icons/vscode_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:glassmorphism_ui/glassmorphism_ui.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:iconify_flutter/iconify_flutter.dart';
+import 'package:iconify_flutter/icons/ic.dart';
 
 class ItemDetails extends StatefulWidget {
   final CoffeeItem cItem;
-  const ItemDetails({super.key,  required this.cItem});
+  const ItemDetails({super.key, required this.cItem});
 
   @override
   State<ItemDetails> createState() => _ItemDetailsState();
@@ -23,62 +29,220 @@ class _ItemDetailsState extends State<ItemDetails> {
             width: screenWidth,
             color: Colors.black,
           ),
-          Hero(tag: widget.cItem.itemImg.toString(), 
-          child: Container(
-            height: (screenHeight/2) + 70.0,
-            width: screenWidth,
-            decoration: BoxDecoration(
-              image: DecorationImage(image: AssetImage(widget.cItem.itemImg.toString()),
-              fit: BoxFit.cover)
-            ),
-          )),
+          Hero(
+              tag: widget.cItem.itemImg.toString(),
+              child: Container(
+                height: (screenHeight / 2) + 70.0,
+                width: screenWidth,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage(widget.cItem.itemImg.toString()),
+                        fit: BoxFit.cover)),
+              )),
           Positioned(
-            top: 35.0,
-            left: 10.0,
-            child: Container(
-              color: Colors.transparent,
-              height: 50.0,
-              width: screenWidth - 20.0,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.of(context).pop();
-                    },
-                    child: Container(
-                      height: 45.0,
-                      width: 45.0,
-                      decoration: BoxDecoration(
-                        color: Color(0xFF14181D),
-                        border: Border.all(color: Color(0xFF322B2E), width: 1.0),
-                        borderRadius: BorderRadius.circular(15.0)
+              top: 35.0,
+              left: 10.0,
+              child: Container(
+                color: Colors.transparent,
+                height: 50.0,
+                width: screenWidth - 20.0,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Container(
+                        height: 45.0,
+                        width: 45.0,
+                        decoration: BoxDecoration(
+                            color: Color(0xFF14181D),
+                            border: Border.all(
+                                color: Color(0xFF322B2E), width: 1.0),
+                            borderRadius: BorderRadius.circular(15.0)),
+                        child: Icon(
+                          Icons.arrow_back_ios_new_rounded,
+                          color: Color(0xFF525154),
+                          size: 17.0,
+                        ),
                       ),
-                      child: Icon(Icons.arrow_back_ios_new_rounded,
-                      color: Color(0xFF525154),
-                      size: 17.0,),
                     ),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      //Navigator.of(context).pop();
-                    },
-                    child: Container(
-                      height: 45.0,
-                      width: 45.0,
-                      decoration: BoxDecoration(
-                        color: Color(0xFF14181D),
-                        border: Border.all(color: Color(0xFF322B2E), width: 1.0),
-                        borderRadius: BorderRadius.circular(15.0)
+                    GestureDetector(
+                      onTap: () {
+                        //Navigator.of(context).pop();
+                      },
+                      child: Container(
+                        height: 45.0,
+                        width: 45.0,
+                        decoration: BoxDecoration(
+                            color: Color(0xFF14181D),
+                            border: Border.all(
+                                color: Color(0xFF322B2E), width: 1.0),
+                            borderRadius: BorderRadius.circular(15.0)),
+                        child: Icon(
+                          Icons.favorite_rounded,
+                          color: Color(0xFF525154),
+                          size: 17.0,
+                        ),
                       ),
-                      child: Icon(Icons.favorite_rounded,
-                      color: Color(0xFF525154),
-                      size: 17.0,),
+                    )
+                  ],
+                ),
+              )),
+          Positioned(
+            top: (screenHeight / 2) - 30.0,
+            child: GlassContainer(
+              height: 150.0,
+              width: screenWidth,
+              blur: 4,
+              border: Border.fromBorderSide(BorderSide.none),
+              borderRadius: BorderRadius.circular(30.0),
+              color: Colors.black.withOpacity(0.6),
+              child: Container(
+                height: 140.0,
+                width: screenWidth,
+                child: Row(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.only(left: 25.0),
+                      height: 140.0,
+                      width: (screenWidth - 20.0) / 2,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            widget.cItem.title!,
+                            style: GoogleFonts.sourceSansPro(
+                                color: Colors.white,
+                                fontSize: 25.0,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            widget.cItem.subtitle!,
+                            style: GoogleFonts.sourceSansPro(
+                              color: Color(0xFFADADAD),
+                              fontSize: 17.0,
+                            ),
+                          ),
+                          SizedBox(height: 10.0),
+                          Row(
+                            children: [
+                              Icon(Icons.star,
+                                  color: ColorPalette().coffeeSelected,
+                                  size: 22.0),
+                              SizedBox(width: 10.0),
+                              Text(
+                                widget.cItem.rating.toString(),
+                                style: GoogleFonts.sourceSansPro(
+                                    color: Colors.white,
+                                    fontSize: 17.0,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              SizedBox(width: 10.0),
+                              Text(
+                                '(6,986)',
+                                style: GoogleFonts.sourceSansPro(
+                                  color: Color(0xFFADADAD),
+                                  fontSize: 15.0,
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
                     ),
-                  )
-                ],
+                    Container(
+                      padding: EdgeInsets.only(left: 25.0),
+                      height: 140.0,
+                      width: (screenWidth - 20.0) / 2,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Container(
+                                height: 50.0,
+                                width: 50.0,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10.0),
+                                    color: Color(0xFF0F1419)),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Iconify(
+                                      VscodeIcons.file_type_coffeescript,
+                                      color: ColorPalette().coffeeSelected,
+                                      size: 21.0,
+                                    ),
+                                    SizedBox(height: 2.0),
+                                    Text(
+                                      'Coffee',
+                                      style: GoogleFonts.sourceSansPro(
+                                          color: Color(0xFF909193),
+                                          fontSize: 12.0),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              Container(
+                                height: 50.0,
+                                width: 50.0,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10.0),
+                                    color: Color(0xFF0F1419)),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Iconify(
+                                      Ic.round_water_drop,
+                                      color: ColorPalette().coffeeSelected,
+                                      size: 21.0,
+                                    ),
+                                    SizedBox(height: 2.0),
+                                    Text(
+                                      'Milk',
+                                      style: GoogleFonts.sourceSansPro(
+                                          color: Color(0xFF909193),
+                                          fontSize: 12.0),
+                                    )
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                          SizedBox(height: 10.0),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                height: 35.0,
+                                width: 120.0,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10.0),
+                                    color: Color(0xFF0F1419)),
+                                child: Center(
+                                  child: Text('Medium Roasted',
+                                  style: GoogleFonts.sourceSansPro(
+                                    color: Color(0xFF909193),
+                                    fontSize: 12.0
+                                  ),)
+                                )
+                              
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ),
-            ))
+            ),
+          )
         ],
       ),
     );
